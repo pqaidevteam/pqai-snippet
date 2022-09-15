839fdd71fcb4b4eeb8c9271c2faa5b321eaf93c7
Mayank Rakesh <33080454+mayank6255@users.noreply.github.com>
2022-09-15 12:58:14 +0530

    Revert "Update reranking_srv.py"
    This reverts commit 227cb6e50613ebd07d45b6662c83269bd470efe3.

--- core/reranking_srv.py
+++ core/reranking_srv.py
@@ -1,6 +1,3 @@
-"""
-Reranking Service
-"""
 import os
 import requests
 
@@ -9,22 +6,17 @@ assert isinstance(RERANKING_SRV_ENDPOINT, str)
 
 
 class CustomRanker:
-    """Runs CustomRanker model from PQAI Reranker"""
-    endpoint = RERANKING_SRV_ENDPOINT
 
+    endpoint = RERANKING_SRV_ENDPOINT
+    
     @classmethod
     def rank(cls, query, docs):
-        """The rank function takes a query and a list of documents as input. It returns the ranks of the documents in relation to the query.
-
-        Args:
-            cls: Used to Access the class variables.
-            query (str): Pass the query string to the reranking service.
-            docs (str): The documents to be ranked.
-        Returns:
-            list(): A list of ranks.
-        """
         url = f"{cls.endpoint}/rerank"
-        params = {"model": "custom-ranker", "query": query, "docs": docs}
+        params = {
+            "model": "custom-ranker",
+            "query": query,
+            "docs": docs
+        }
         response = requests.post(url, json=params)
         if response.status_code != 200:
             raise Exception("Reranking service failure")
@@ -33,22 +25,17 @@ class CustomRanker:
 
 
 class ConceptMatchRanker:
-    """Runs CustomMatchRanker model from PQAI Reranker"""
-    endpoint = RERANKING_SRV_ENDPOINT
 
+    endpoint = RERANKING_SRV_ENDPOINT
+    
     @classmethod
     def score(cls, query, doc):
-        """Takes a query and a document as input,and returns the score of the document for that query.
-
-        Args:
-            cls: Used to Access the class variables.
-            query (str): Specify the query string.
-            doc (str): The text of a document that is being scored.
-        Returns:
-             (float): The score of the document.
-        """
         url = f"{cls.endpoint}/score"
-        params = {"model": "concept-match-ranker", "query": query, "doc": doc}
+        params = {
+            "model": "concept-match-ranker",
+            "query": query,
+            "doc": doc
+        }
         response = requests.post(url, json=params)
         if response.status_code != 200:
             raise Exception("Reranking service failure")
@@ -57,19 +44,12 @@ class ConceptMatchRanker:
 
     @classmethod
     def rank(cls, query, docs):
-        """The rank function takes a query and a list of documents as input.
-        It returns the ranks of the documents in relation to the query.
-
-        Args:
-            cls: Used to Access the class variables of the class.
-            query (str): Query string.
-            docs (list): The list of documents that are to be reranked.
-
-        Returns:
-            A list of integers, which correspond to the rank of each document in the query.
-        """
         url = f"{cls.endpoint}/rerank"
-        params = {"model": "concept-match-ranker", "query": query, "docs": docs}
+        params = {
+            "model": "concept-match-ranker",
+            "query": query,
+            "docs": docs
+        }
         response = requests.post(url, json=params)
         if response.status_code != 200:
             raise Exception("Reranking service failure")
